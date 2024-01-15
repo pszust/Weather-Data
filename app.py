76cd663c7a5da2870cf186925cf89bf5624362e0
piotr <szustakpio@gmail.com>
2024-01-15 18:47:47 +0100

    added main functionality

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import dash
 # import dash_core_components as dcc
 # import dash_html_components as html
 from matplotlib import pyplot as plt
-from dash import html, dcc
+from dash import html, dcc, ctx
 from dash.dependencies import Input, Output, State
 import pandas as pd
 from datetime import datetime
@@ -11,9 +11,7 @@ import re
 import numpy as np
 
 
-data_dir = 'd:/Katalog 1/Projekty/Weather/data/'
-data_dir = ''
-
+# useful variables
 months = {
     1: 'sty',
     2: 'lut',
@@ -28,13 +26,38 @@ months = {
     11: 'lis',
     12: 'gru'
 }
+introText = """
+Prosta aplikacja pozwalająca na tworzenie wykresów historycznych danych pogodowych z obszaru Polski, 
+zbieranych przez IMGW od około 1960 roku. Aplikacja umożliwia eksplorację dobowych średnich temperatur, 
+opadów oraz wysokości pokrywy śnieżnej. Prześledź zmiany pogodowe na przestrzeni lat za pomocą intuicyjnych 
+wykresów i analizuj różne aspekty klimatyczne. Dzięki tej aplikacji z łatwością zanurzysz się w bogatej 
+historii danych meteorologicznych z terenu Polski.
+"""
+selectParametersText = """
+Zdefiniuj parametry wykresu, dostosowując analizę do własnych potrzeb. Możesz wybrać grupowanie danych 
+poprzez obliczanie średniej i odchylenia standardowego dla poszczególnych lat lub miesięcy. 
 
+Gdy grupujesz dane po roku, masz możliwość wyboru konkretnych miesięcy, które chcesz uwzględnić 
+podczas obliczeń. Jeśli natomiast wybierasz grupowanie po miesiącu, możesz określić zakres lub 
+zakresy lat, które chcesz uwzględnić w analizie.
+
+Dodatkowo, istnieje opcja wyświetlenia dobowych pomiarów bez grupowania, z możliwością zaznaczenia 
+konkretnego zakresu dat. 
+
+Dane będą pochodzić z zaznaczonych przez użytkownika konkretnych stacji 
+pogodowych na mapie, co umożliwi jeszcze dokładniejszą personalizację analizy klimatu według 
+preferencji i lokalnych warunków atmosferycznych.
+"""
+
+# read data from static csv files
+data_dir = 'd:/Katalog 1/Projekty/Weather/data/'
+data_dir = ''
 latlon = pd.read_csv(data_dir + 'latlon.csv')
-
 df = pd.read_csv(data_dir + 'all.csv')
 df['date'] = df['date'].apply(lambda row: datetime.strptime(row, '%Y-%m-%d'))
 df['month'] = df['date'].apply(lambda row: row.month)
 df['year'] = df['date'].apply(lambda row: row.year)
+
 
 def yearly_figure(cities, selectedMonths, dataType, trendline=None):
     # select data and calculate mean and std
@@ -262,37 +285,48 @@ def make_map_chart():
     fig['layout']['geo']['lataxis'] = dict(range=[-rng, rng])
     fig['layout']['dragmode'] = 'lasso'
     fig.update_layout(
-        autosize=False,
-        width=400,
-        height=400,
+        # autosize=False,
+        # width=400,
+        # height=400,
         margin=dict(l=0, r=0, b=0, t=0, pad=0),
-        paper_bgcolor="LightSteelBlue",
+        # paper_bgcolor="LightSteelBlue",
     )
     return fig
 
 
 mapPlot = make_map_chart()
 
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, prevent_initial_callbacks='initial_duplicate')
 server = app.server
 app.layout = html.Div(
     className='container',
     children=[
         html.Div(
             className='column',
-            style={'width': '25%'},
+            style={'width': '99%'},
             children=[
-                html.P("Simple Dash App with Plots!"),
-                dcc.Graph(figure=mapPlot, style={
-                    'width': '100%'}, id='scatter-plot')
-            ]),
+                html.H1('Historyczne dane pogodowe'),
+                html.Br(),
+                html.P(introText)
+            ]
+        ),
         html.Div(
             className='column',
-            style={'width': '75%'},
+            style={'width': '24%', 'min-height': '400px'},
             children=[
-                html.P("Plot parameters!"),
-                html.P(
-                    "ruugvfn wirgj iuefb huierhn viuyefh viuj ndfibjn iufh bfi dhiusffdh giusf hg"),
+                html.H3("Mapa stacji pogodowych"),
+                html.P('Aby wybrać stacje pogodowe, zaznacz je na mapie metodą na \'lasso\'. Podwójne klinknięcie zaznacza'
+                       ' wszystkie stacje. Uwaga: kopletność danych nie jest pełna dla każdej stacji.'),
+                dcc.Graph(figure=mapPlot, style={
+                    'width': '100%'}, id='scatter-plot')
+            ]
+        ),
+        html.Div(
+            className='column',
+            style={'width': '74%', 'min-height': '400px'},
+            children=[
+                html.H3('Parametry wykresu'),
+                html.P(selectParametersText),
 
                 # dropdown to select which plot setting should be displayed
                 dcc.Dropdown(options=[
@@ -306,60 +340,114 @@ app.layout = html.Div(
                     id='settings-yearly',
                     style={'display': 'none'},  # initial visibility
                     children=[
-                        html.P('Wykres grupowany po latach'),
+                        # html.P('Wykres grupowany po latach'),
                         html.P('Wybrane miesiące:'),
                         dcc.Checklist(
                             options=[{'label': val, 'value': key}
                                      for key, val in months.items()],
-                            value = list(months.keys()),
+                            value=list(months.keys()),
                             id='selected-months',
                             style={'display': 'flex', 'flexWrap': 'wrap'},
                             inputStyle={'marginRight': '10px'},
-                            labelStyle={'display': 'inline-block', 'marginRight': '10px', 'marginBottom': '10px'}
+                            labelStyle={'display': 'inline-block',
+                                        'marginRight': '10px', 'marginBottom': '10px'}
                         ),
-                        html.Button('Generate Yearly Plot', id='plot-yearly-button')
+                        html.Button('Wygeneruj wykres (grupowanie roczne)',
+                                    id='plot-yearly-button')
                     ]
                 ),
                 html.Div(
                     id='settings-monthly',
                     style={'display': 'none'},  # initial visibility
                     children=[
-                        html.P('Wykres grupowany po miesiącach'),
-                        dcc.RadioItems(
-                            id='radio-buttons',
-                            options=[
-                                {'label': 'Option 1', 'value': 'option1'},
-                                {'label': 'Option 2', 'value': 'option2'},
-                                {'label': 'Option 3', 'value': 'option3'}
-                            ],
-                            value='option1',  # default selected value
-                        )]
+                        # html.P('Wykres grupowany po miesiącach'),
+                        html.Div(
+                            id='data-range-sliders-monthly',
+                            children=[
+                                dcc.RangeSlider(1960, 2023, 1, value=[
+                                                2000, 2005], id='0', marks={year: str(year) for year in range(1960, 2021, 5)})
+                            ]
+                        ),
+                        html.Button('Dodaj zakres',
+                                    id='add-range', n_clicks=0),
+                        html.Button('Usuń zakres',
+                                    id='rem-range', n_clicks=0),
+                        html.Br(),
+                        html.Br(),
+                        html.Button('Wygeneruj wykres (grupowanie miesięczne)',
+                                    id='plot-monthly-button')
+                    ]
                 ),
                 html.Div(
                     id='settings-daily',
                     style={'display': 'none'},  # initial visibility
                     children=[
-                        html.P('Wykres grupowany po dniach'),
-                        dcc.RadioItems(
-                            id='radio-buttons',
-                            options=[
-                                {'label': 'Option 1', 'value': 'option1'},
-                                {'label': 'Option 2', 'value': 'option2'},
-                                {'label': 'Option 3', 'value': 'option3'}
-                            ],
-                            value='option1',  # default selected value
-                        )]
+                        # html.P('Wykres grupowany po dniach'),
+                        dcc.RangeSlider(1960, 2023, 1/4, value=[2000, 2005], id='data-range-daily-slider', marks={
+                                        year: str(year) for year in range(1960, 2021, 5)}),
+                        html.Div(id='data-range-daily-text'),
+                        html.Br(),
+                        html.Br(),
+                        html.Button('Wygeneruj wykres (pomiary dobowe)',
+                                    id='plot-daily-button')
+                    ]
                 ),
+                html.Br(),
+                html.P('Wybierz rodzaj danych do wykresu:'),
+                dcc.RadioItems(
+                    options={'tmin': 'T-min', 'tmax': 'T-max', 'tmean': 'T-średnia', 'prec': 'Opady', 'tmin_grnd': 'T-min-grunt', 'snow': 'Pokrywa śnieżna'}, inline=True, value='tmean', id='data-type-selector'
+                ),
+                html.Br(),
                 html.Div(id='selected-points-output')
-            ]),
+            ]
+        ),
         html.Div(
             className='column',
-            style={'width': '60%', 'background-color': 'lime'},
+            style={'width': '100%', 'height': '500px'},
             children=[
-                html.Div(id='main-plot-area')
-            ])
+                # html.Div(id='main-plot-area', style={'width': '50%', 'margin': 'auto', 'height': '500px'})
+                dcc.Graph(
+                    id='main-plot-area', style={'width': '65%', 'aspect-ratio': '1.90', 'margin': 'auto'})
+            ]
+        )
     ]
 )
+
+
+@app.callback(
+    Output('data-range-daily-text', 'children'),
+    [Input('data-range-daily-slider', 'value')]
+)
+def update_daily_range_text(dailyRange):
+    # converts date from year (float) to YYYY/MM format (where 01 is January)
+    dataStart, dataEnd = (
+        f'{int(d)}/{str(round(d%1*12+1)).zfill(2)}' for d in dailyRange)
+    return f'Wybrany zakres: do {dataStart} do {dataEnd}.'
+
+
+@app.callback(
+    Output('data-range-sliders-monthly', 'children'),
+    [Input('add-range', 'n_clicks'),
+     Input('rem-range', 'n_clicks')],
+    [State('data-range-sliders-monthly', 'children')]
+)
+def update_monthly_range_sliders(add_clicks, remove_clicks, current_children):
+    # id of the element that triggered callback, either add-range or rem-range
+    changed_id = ctx.triggered_id
+    # add new rangeslider
+    if changed_id == 'add-range' and len(current_children) < 15:
+        new_slider = dcc.RangeSlider(
+            min=1960,
+            max=2023,
+            step=1,
+            value=[2000, 2005],
+            id={'type': 'range-slider', 'index': add_clicks},
+            marks={year: str(year) for year in range(1960, 2024, 5)}
+        )
+        current_children.append(new_slider)
+    elif len(current_children) > 1:  # remove last range slider
+        current_children.pop()
+    return current_children
 
 
 @app.callback(
@@ -378,8 +466,6 @@ def update_visibility(selectedVisibility):
     if selectedVisibility == 'yearly':
         return styleShow, styleHide, styleHide
 
-# callback to capture selected points
-
 
 @app.callback(
     Output('selected-points-output', 'children'),
@@ -389,17 +475,18 @@ def display_selected_data(selected_data):
     if selected_data is not None:
         selected_points = selected_data['points']
         miasta = [point['text'] for point in selected_points]
-        return f"Wybrane stacje: {', '.join(m.capitalize() for m in miasta)}"
+        return f"Wybrane stacje pogodowe: {', '.join(m.capitalize() for m in miasta)}"
     else:
-        return 'Wszystkie stacje są zaznaczone'
+        return 'Wszystkie stacje pogodowe są zaznaczone'
 
 
 @app.callback(
-    Output('main-plot-area', 'children'),
+    Output('main-plot-area', 'figure', allow_duplicate=True),
     [Input('plot-yearly-button', 'n_clicks')],
-    [State('scatter-plot', 'selectedData'), State('selected-months', 'value')]
+    [State('scatter-plot', 'selectedData'), State('selected-months', 'value'),
+     State('data-type-selector', 'value')]
 )
-def update_yearly_plot(nClicks, selectedData, selectedMonths):
+def update_yearly_plot(nClicks, selectedData, selectedMonths, dataType):
     if nClicks is None:
         return dash.no_update
     else:
@@ -410,8 +497,60 @@ def update_yearly_plot(nClicks, selectedData, selectedMonths):
         else:
             miasta = df['name'].values
 
-        fig = yearly_figure(miasta, selectedMonths, 'tmean', trendline=5)
-        return dcc.Graph(figure=fig)
+        fig = yearly_figure(miasta, selectedMonths, dataType, trendline=5)
+        # return dcc.Graph(figure=fig)
+        return fig
+
+
+@app.callback(
+    Output('main-plot-area', 'figure', allow_duplicate=True),
+    [Input('plot-monthly-button', 'n_clicks')],
+    [State('scatter-plot', 'selectedData'),
+     State('data-range-sliders-monthly', 'children'),
+     State('data-type-selector', 'value')]
+)
+def update_monthly_plot(nClicks, selectedData, dateRangeSliders, dataType):
+    # get the ranges selected in all data range sliders
+    dataRanges = [slider['props']['value'] for slider in dateRangeSliders]
+    if nClicks is None:
+        return dash.no_update
+    else:
+        # create figure based on selected data points and data ranges
+        if selectedData is not None:
+            selectedPoints = selectedData['points']
+            miasta = [point['text'] for point in selectedPoints]
+        else:
+            miasta = df['name'].values
+
+        fig = monthly_figure(miasta, dataRanges, dataType, trendline=5)
+        return fig
+
+
+@app.callback(
+    Output('main-plot-area', 'figure', allow_duplicate=True),
+    [Input('plot-daily-button', 'n_clicks')],
+    [State('scatter-plot', 'selectedData'),
+     State('data-range-daily-slider', 'value'),
+     State('data-type-selector', 'value')]
+)
+def update_daily_plot(nClicks, selectedData, dailyRange, dataType):
+
+    # converts date from year (float) to datetime
+    dataStart, dataEnd = (datetime(int(d), round(d % 1*12+1), 1)
+                          for d in dailyRange)
+
+    if nClicks is None:
+        return dash.no_update
+    else:
+        # create figure based on selected data points and data ranges
+        if selectedData is not None:
+            selectedPoints = selectedData['points']
+            miasta = [point['text'] for point in selectedPoints]
+        else:
+            miasta = df['name'].values
+
+        fig = daily_figure(miasta, [dataStart, dataEnd], dataType, trendline=5)
+        return fig
 
 
 if __name__ == '__main__':
